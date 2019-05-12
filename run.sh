fb6834f1047f81138181833f86344bc0a6bfb820
Jesper Leerberg Madsen <jesper.leerberg@gmail.com>
2019-05-12 18:51:36 +0200

    Allow startup without table items preloaded

--- run.sh
+++ run.sh
@@ -12,8 +12,17 @@ if [ -d "/opt/dynamodb/data" ]; then
   echo "Data found. Preparing database using the awscli"
   echo "*************************************************************************"
   sleep 2
-  aws dynamodb create-table --cli-input-json file:///opt/dynamodb/data/table.json --endpoint-url http://localhost:8000
-  aws dynamodb batch-write-item --request-items file:///opt/dynamodb/data/batchData.json --endpoint-url http://localhost:8000
+  if [ -f "/opt/dynamodb/data/table.json" ]; then
+    aws dynamodb create-table --cli-input-json file:///opt/dynamodb/data/table.json --endpoint-url http://localhost:8000
+  else
+    echo "No table.json file found in the /opt/dynamodb/data folder. Exiting..."
+    exit 3 
+  fi
+  if [ -f "/opt/dynamodb/data/batchData.json" ]; then
+    aws dynamodb batch-write-item --request-items file:///opt/dynamodb/data/batchData.json --endpoint-url http://localhost:8000
+  else
+    echo "No batchData.json file found in the /opt/dynamodb/data folder. Starting Dynamo without any preloaded items."
+  fi
 else
   echo "*************************************************************************"
   echo "No datafolder found at path /opt/dynamodb/data. Starting a clean DynamoDB"
